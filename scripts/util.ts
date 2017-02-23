027a53cb2f0edcac8e22e40c6ffd6406d42a0367
jlgrossman <josh.l.grossman@gmail.com>
2017-02-22 22:43:28 -0600

    added js util lib

---
+++ scripts/util.ts
@@ -0,0 +1,95 @@
+function ajax(obj) {
+    if (!obj.url) return;
+    var params: any = [];
+    for (var i in obj.params) params.push(encodeURIComponent(i) + "=" + encodeURIComponent(obj.params[i]));
+    params = params.join("&");
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", obj.url, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            try {
+                if (obj.success) {
+                    obj.success(
+                        (obj.responseType && obj.responseType.toLowerCase() == 'text') ?
+                        xhr.responseText :
+                        JSON.parse(xhr.responseText)
+                    );
+                }
+            } catch (e) {
+                if (obj.error) obj.error(e);
+            }
+        }
+    };
+    xhr.send(params);
+}
+
+function $(arg) {
+    var a;
+    if (arg instanceof Array) a = arg;
+    else a = (/<[a-zA-Z0-9]+>/.test(arg)) ? [document.createElement(arg.replace(/<|>/g, ""))] : arg instanceof HTMLElement ? [arg] : document.querySelectorAll(arg);
+    a['each'] = function(f) {
+        for (var i = 0; i < this.length; i++) f(this[i]);
+        return this;
+    };
+    a['append'] = function(e) {
+        return this.each(function(n) {
+            n.appendChild((e instanceof Array) ? e[0] : e);
+        });
+    };
+    a['find'] = function(s) {
+        var a = [];
+        this.each(function(n) {
+            a.push(...n.querySelectorAll(s));
+        });
+        return $(a);
+    };
+    a['remove'] = function() {
+        return this.each(function(n) {
+            n.parentNode.removeChild(n);
+        });
+    };
+    a['parent'] = function() {
+        return $(this[0].parentNode);
+    };
+    a['on'] = function(e, f) {
+        return this.each(function(n) {
+            n.addEventListener(e, f);
+        });
+  };
+    a['addClass'] = function(c) {
+        return this.each(function(n) {
+            n.classList.add(c);
+        });
+    };
+    a['removeClass'] = function(c) {
+        return this.each(function(n) {
+            n.classList.remove(c);
+        });
+    };
+    a['toggleClass'] = function(c) {
+        return this.each(function(n) {
+            n.classList.toggle(c);
+        });
+    };
+    a['hasClass'] = function(c) {
+        for (var i = 0; i < this.length; i++)
+            if (this[i].classList.contains(c)) return true;
+        return false;
+    };
+    a['text'] = function(t) {
+        return t == undefined ? (this[0].innerText || this[0].value) : this.each(function(n) {
+            "value" in n ? n.value = t : n.appendChild(document.createTextNode(t));
+        });
+    };
+    a['data'] = function(m, v) {
+        return !v ? this[0].dataset[m] : this.each(function(n) {
+            n.dataset[m] = v;
+        });
+    };
+    return a;
+}
+
+function ready(f) {
+    window.addEventListener('load', f);
+};
