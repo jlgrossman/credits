abbbc52f6e261fd090e6c81a9d19ec2898edf013
jlgrossman <josh.l.grossman@gmail.com>
2017-02-23 21:06:34 -0600

    added $isLoggedIn and $isAdmin vars

--- php/user-session.php
+++ php/user-session.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
+$isLoggedIn = isset($_SESSION['id'])
 $user = array(
-  'name' => $_SESSION['user'],
-  'id' => intval($_SESSION['id'])
+  'name' => ($isLoggedIn ? $_SESSION['user'] : 'none'),
+  'id' => ($isLoggedIn ? intval($_SESSION['id'] : 0),
 );
+$isAdmin = ($user['name'] == 'Admin' || $user['name'] == 'Jarrod')
  ?>
