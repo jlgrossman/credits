93f1f28691966d49e9825ab50b2b1fe3bda58a2f
jlgrossman <josh.l.grossman@gmail.com>
2017-02-23 21:29:23 -0600

    fixed syntax errors

--- php/user-session.php
+++ php/user-session.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$isLoggedIn = isset($_SESSION['id'])
+$isLoggedIn = isset($_SESSION['id']);
 $user = array(
   'name' => ($isLoggedIn ? $_SESSION['user'] : 'none'),
-  'id' => ($isLoggedIn ? intval($_SESSION['id'] : 0),
+  'id' => ($isLoggedIn ? intval($_SESSION['id']) : 0),
 );
-$isAdmin = ($user['name'] == 'Admin' || $user['name'] == 'Jarrod')
+$isAdmin = ($user['name'] == 'Admin' || $user['name'] == 'Jarrod');
  ?>
