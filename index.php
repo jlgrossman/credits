6d9618296346865db60bfa6b4ce7ee3bbbb1fd94
jlgrossman <josh.l.grossman@gmail.com>
2017-03-08 20:38:07 -0600

    fixed bug of overlapping divs due to float and clearfix

--- index.php
+++ index.php
@@ -54,8 +54,10 @@ include_once 'php/utils.php';
             <h2>Transfer Credits</h2>
             <div class="transfer form">
               <div class="error-msg"></div>
-              <input class="transfer-name" type="text" placeholder="Payee's Name" name="payee" />
-              <input class="transfer-amount" type="text" placeholder="Credits" name="transfer" />
+              <div class="transfer-details">
+                <input class="transfer-name" type="text" placeholder="Payee's Name" name="payee" />
+                <input class="transfer-amount" type="text" placeholder="Credits" name="transfer" />
+              </div>
               <div class="transfer-message-container">
                 <textarea class="transfer-message" name="message" placeholder="Include a message..." maxlength="140"></textarea>
                 <div class="transfer-character-count">140/140</div>
