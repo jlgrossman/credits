c54f64093c22d43a8c1ba3b8b9fbbe84f1b94b79
jlgrossman <josh.l.grossman@gmail.com>
2017-03-10 19:09:35 -0600

    change login error msg

--- php/login.php
+++ php/login.php
@@ -5,7 +5,7 @@ $name = trim($_POST['name']);
 $pw = trim($_POST['pw']);
 
 if(strlen($name) < $MIN_USERNAME_LENGTH || strlen($pw) < $MIN_PASSWORD_LENGTH){
-  exit('{"success":false, "msg":"Invalid display name or username"}');
+  exit('{"success":false, "msg":"Invalid login"}');
 }
 
 $query = $connection->prepare('SELECT id FROM users WHERE username = ? AND password = ?');
