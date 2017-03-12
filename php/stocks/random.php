e0ea3788c8a94330d4faee5fca9be63e45dc7b75
jlgrossman <josh.l.grossman@gmail.com>
2017-03-12 18:23:32 -0500

    Minimum Rand stock value

--- php/stocks/random.php
+++ php/stocks/random.php
@@ -3,6 +3,8 @@ $previousRand = intval($previousRand);
 
 $rand = intval(((mt_rand() / mt_getrandmax()) * 30 + $previousRand)/2);
 
+if($rand < 3) $rand = 3;
+  
 $query = $connection->prepare('UPDATE stocks SET value = ? WHERE name = "RAND"');
 $query->bind_param('i', $rand);
 $query->execute();
