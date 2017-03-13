12b244c2f363634bbbcb390800047baaeecf8535
jlgrossman <josh.l.grossman@gmail.com>
2017-03-13 05:35:56 -0500

    lowered rewards of random stock

--- php/stocks/random.php
+++ php/stocks/random.php
@@ -1,10 +1,10 @@
 <?
 $previousRand = intval($previousRand);
 
-$rand = intval(((mt_rand() / mt_getrandmax()) * 30 + $previousRand)/2);
+$rand = intval(((mt_rand() / mt_getrandmax()) * 10 + $previousRand)/2);
 
-if($rand < 3) $rand = 3;
-  
+if($rand < 1) $rand = 1;
+
 $query = $connection->prepare('UPDATE stocks SET value = ? WHERE name = "RAND"');
 $query->bind_param('i', $rand);
 $query->execute();
