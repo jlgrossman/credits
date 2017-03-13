404111088d7be6052f288903069611d2ae535570
jlgrossman <josh.l.grossman@gmail.com>
2017-03-13 18:16:19 -0500

    modified random algorithm

--- php/stocks/random.php
+++ php/stocks/random.php
@@ -1,9 +1,12 @@
 <?
 $previousRand = intval($previousRand);
 
-$rand = intval(((mt_rand() / mt_getrandmax()) * 10 + $previousRand)/2);
+$rand = (mt_rand() / mt_getrandmax()) - 0.5;
+$scale = (mt_rand() / mt_getrandmax()) > 0.95 ? 10 : 3.5;
+$rand = $previousRand + intval(round($rand * $scale)); // usually move by 0 or 1, sometimes 2
 
 if($rand < 1) $rand = 1;
+else if($rand > 15) $rand = 15;
 
 $query = $connection->prepare('UPDATE stocks SET value = ? WHERE name = "RAND"');
 $query->bind_param('i', $rand);
