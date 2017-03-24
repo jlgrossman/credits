8338b2f3ee933aa126dafde5815e4dc5841bf73f
jlgrossman <josh.l.grossman@gmail.com>
2017-03-23 21:56:38 -0500

    divide pot, reset pot

--- php/games/lottery/drawing.php
+++ php/games/lottery/drawing.php
@@ -3,8 +3,8 @@ include_once '../../connection.php';
 
 $numbers = array(mt_rand()%10, mt_rand()%10, mt_rand()%10);
 
-$query = $connection->prepare('
-  SELECT pot.value, user.username FROM tickets AS ticket
+$query_select = $connection->prepare('
+  SELECT pot.value, user.username, COUNT(user.id) FROM tickets AS ticket
   INNER JOIN users AS user ON user.id = ticket.user_id
   INNER JOIN pots AS pot ON pot.name = "lottery"
   WHERE (
@@ -13,21 +13,29 @@ $query = $connection->prepare('
     ticket.number2 = ?
   )
 ');
-$query->bind_param('iii', $numbers[0], $numbers[1], $numbers[2]);
-$query->execute();
-$query->bind_result($value, $userID);
-$success = $query->fetch();
-$query->close();
+$query_select->bind_param('iii', $numbers[0], $numbers[1], $numbers[2]);
+$query_select->execute();
+$query_select->bind_result($value, $userID, $numberOfWinners);
 
-if($success){
+$query_update = $connection->prepare('
+  UPDATE pots, users
+  SET
+  pots.value = 0,
+  users.credits = users.credits + ?
+  WHERE
+  pots.name = "lottery" AND users.id = ?
+');
+$query_update->bind_param('ii', $value, $userID);
 
-  $query = $connection->prepare('UPDATE users SET credits = credits + ? WHERE id = ?');
-  $query->bind_param('ii', $value, $userID);
-  $query->execute();
-  $query->close();
-
+while($query_select->fetch()){
+  $value = $value / $numberOfWinners;
+  $query_update->execute();
 }
 
+$query_select->close();
+$query_update->close();
+
+$connection->query('UPDATE pots SET pots.value = pots.value + 100 WHERE pots.name="lottery"');
 $connection->query('DELETE FROM tickets WHERE 1'); // delete all tickets
 $connection->close();
 
