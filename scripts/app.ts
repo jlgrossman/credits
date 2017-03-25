8f53a393bef4678f62eaa9ee388c199bc6fff75f
jlgrossman <josh.l.grossman@gmail.com>
2017-03-24 21:07:35 -0500

    added JS form validation messages

--- scripts/app.ts
+++ scripts/app.ts
@@ -93,7 +93,7 @@ ready(function(){
     const amount:number = parseInt($transferAmount.text());
     const msg:string = $transferMessage.text();
 
-    if(name.length && !isNaN(amount)){
+    if(name.length && !isNaN(amount)) {
       $transferSubmit.addClass(LOADING);
       transferInProgress = true;
       ajax({
@@ -110,12 +110,21 @@ ready(function(){
           transferInProgress = false;
           if(data.success){
             $creditCount.text(parseInt($creditCount.text()) - amount);
+            $transferError.removeClass(OPEN);
             loadTransactions();
           } else {
             $transferError.text(data.msg).addClass(OPEN);
           }
+        },
+        error: function(data){
+          $transferAmount.add($transferMessage).add($transferUsername).text('');
+          $transferSubmit.removeClass(LOADING);
+          transferInProgress = false;
+          $transferError.text('Transfer failed').addClass(OPEN);
         }
       });
+    } else {
+      $transferError.text('Invalid data').addClass(OPEN);
     }
   }
 
@@ -183,6 +192,7 @@ ready(function(){
        },
        success: function(data){
          if(data.success){
+           $loginError.removeClass(OPEN);
            window.location.reload();
          } else {
            $loginSubmit.removeClass(LOADING);
@@ -190,6 +200,8 @@ ready(function(){
          }
        }
      });
+   } else {
+     $loginError.text('All fields required').addClass(OPEN);
    }
  }
 
