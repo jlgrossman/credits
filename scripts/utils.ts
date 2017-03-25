0d0e0ab6e0cfdac6926126eaf28b5443ea9450fc
jlgrossman <josh.l.grossman@gmail.com>
2017-03-24 22:36:00 -0500

    allows for complex html strings within $

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -97,14 +97,19 @@ const $:(arg:any)=>$ = (function(){
         }
       }
     }
+  }
 
+  function stringToHTML(str:string):Node {
+    const temp = document.createElement('template');
+    temp.innerHTML = str;
+    return temp.content.firstChild;
   }
 
   return function $(arg:any):$ {
       var a;
       if(arg.each) return arg;
       else if (arg instanceof Array) a = arg;
-      else a = (/<[a-zA-Z0-9]+>/.test(arg)) ? [document.createElement(arg.replace(/<|>/g, ""))] : arg instanceof HTMLElement ? [arg] : document.querySelectorAll(arg);
+      else a = (typeof arg == 'string' && arg.indexOf('<') >= 0) ? [stringToHTML(arg)] : arg instanceof HTMLElement ? [arg] : document.querySelectorAll(arg);
       a.each = function(f) {
           for (var i = 0; i < this.length; i++) f(this[i],i);
           return this;
