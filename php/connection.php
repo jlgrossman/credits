38e90456060b79ec7f679a151ec4909838b03559
jlgrossman <josh.l.grossman@gmail.com>
2017-03-27 06:02:54 -0500

    short circuit if connection error

--- php/connection.php
+++ php/connection.php
@@ -1,12 +1,14 @@
 <?
 $connection = new mysqli('host', 'username', 'pw', 'db');
+
+if($connection->connect_error){
+    die(json_encode(array('success' => false, 'msg' => $connection->connect_error)));
+}
+
 function exitIfNoKey(){
   if(!(isset($_GET['key']) && $_GET['key'] == 'example')){
     die('{"success": false, "msg": "Invalid key"}');
   }
 }
 
-if($connection->connect_error){
-    die(json_encode(array('success' => false, 'msg' => $connection->connect_error)));
-}
 ?>
