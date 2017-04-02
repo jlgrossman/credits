cc832858f3f9caac6354e0c43010c283cc05ca89
jlgrossman <josh.l.grossman@gmail.com>
2017-04-02 13:43:34 -0500

    prevent submissions while submitting

--- scripts/classes/Component.ts
+++ scripts/classes/Component.ts
@@ -54,12 +54,14 @@ class FormComponent extends Component {
   private _$fields:$;
   private _$submit:$;
   private _$errorMessage:$;
+  private _submitInProgress:boolean;
 
   public constructor(webservice:string, public readonly $form:$, public readonly validate:(fields:Object)=>string, success?:(data:Object)=>void, error?:(msg:string)=>void){
     super(webservice, success, error);
     this._$fields = this.$form.find('.field');
     this._$submit = this.$form.find('.submit');
     this._$errorMessage = this.$form.find('.error-msg');
+    this._submitInProgress = false;
   }
 
   public get $fields():$ { return this._$fields; }
@@ -73,13 +75,15 @@ class FormComponent extends Component {
       fields[$field.data('name')] = $field.text();
     });
 
-    if(validationMessage = this.validate(fields)){
+    if(!this._submitInProgress && (validationMessage = this.validate(fields))){
       this._$submit.addClass(Css.LOADING);
+      this._submitInProgress = true;
       ajax({
         url: this.webservice,
         params: extend(params, fields),
         success: (data) => {
           this._$submit.removeClass(Css.LOADING);
+          this._submitInProgress = false;
           if(data.success) {
             this._$errorMessage.removeClass(Css.OPEN);
             this.success(data);
@@ -90,6 +94,7 @@ class FormComponent extends Component {
         },
         error: (msg) => {
           this._$submit.removeClass(Css.LOADING);
+          this._submitInProgress = false;
           this._$errorMessage.text(msg).addClass(Css.OPEN);
           this.error(msg);
         }
